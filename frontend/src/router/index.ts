bb0a3acf7c02636622c0217413b020781c535d33
MiyazakiAkari <mimrikaakizayami@gmail.com>
2025-11-21 17:27:53 +0900

    register機能実装

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../pages/LoginPage.vue'
 import RegisterPage from '../pages/RegisterPage.vue'
+import HomePage from '../pages/HomePage.vue'
 
 const routes = [
   { path: '/login', component: LoginPage },
   { path: '/register', component: RegisterPage },
+  { path: '/home', component: HomePage },
   { path: "/:pathMatch(.*)*", redirect: "/login" }
 ]
 
